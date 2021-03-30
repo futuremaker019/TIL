d0b04030ae605bfecd1b5b196636453d75335cfd
futuremaker019@gmail.com <ghjung@simmssoft.com>
2021-03-30 14:45:59 +0900

    Javascript/React updated

--- Javascript/React/Udemy/keeper-app/my-app/src/index.js
+++ Javascript/React/Udemy/keeper-app/my-app/src/index.js
@@ -2,3 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+
+
+// 1. Create a new React app
+// 2. Create a App jsx component
+// 3. Create a Header.jsx component that renders a <header> element 
+// to show the Keeper App name in an <h1>
+// 4. Create a Footer.jsx component that renders a <footer> element
+// to show a copyright message in a <p> with a dynamically updated year.
+// 5. Create a Note.jsx component to show a <div> element with a <h1>
+// for a little and a <p> for the content
+// 6. Make sure that the final website is styled like the example show here
+// https://w00gz.csb.app/
