30a7312d146b14626a3fe9f2ef0c8f4cb299aa45
Goohyun Jung <futuremaker019@gmail.com>
2023-09-10 15:27:22 +0900

    문제 설명 추가

--- Algorithm/Java/youtube/code/1.greedy/MultiplyOrPlus.java
+++ Algorithm/Java/youtube/code/1.greedy/MultiplyOrPlus.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class MultiplyOrPlus {
+
+    /**
+     * 곱하기 혹은 더하기
+
+        각 자리 숫자(0 ~ 9)로만 이루어진 문자열 S가 주어졌을때 
+        왼쩍부터 오른쪽으로 하나씩 모든 숫자를 학인하며 숫자 사이에 x 혹은 + 연산자를 넣어
+        결과적으로 만들어진 가장 큰 수를 구하는 프로그램을 작성한다.
+     */
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
