62e3b8b0a8b0497bbbfdf5c6230dc89a8d004af1
Goohyun Jung <futuremaker019@gmail.com>
2023-09-26 23:15:27 +0900

    practice added

--- Algorithm/Java/inflean/code/DFS/pratice/day01/Virus.java
+++ Algorithm/Java/inflean/code/DFS/pratice/day01/Virus.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Virus {
-    
+
+    static int MAX = 100;
+    static boolean[][] map;
+    static boolean[] visited;
+    static int T;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        
+
+    }
 }
