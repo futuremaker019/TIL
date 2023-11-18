cad7b876a29a04295eb83745df4468126003553f
Goohyun Jung <futuremaker019@gmail.com>
2023-11-19 02:18:50 +0900

    Algorithm

--- Algorithm/Python/lecture/inflearn/Basic-lecture/3.탐색-시뮬레이션/AA.py
+++ Algorithm/Python/lecture/inflearn/Basic-lecture/3.탐색-시뮬레이션/AA.py
@@ -1,19 +1,10 @@
-n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(7)]
+count = 0
+for i in range(7):
+    for j in range(3):
+        if all(arr[i][j + k] == arr[i][j + 5 - k - 1] for k in range(2)):
+            count += 1
+        if all(arr[j + k][i] == arr[j + 5 - k - 1][i] for k in range(2)):
+            count += 1
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-a.insert(0, [0]*n)
-a.append([0]*n)
-for x in a:
-  x.insert(0, 0)
-  x.append(0)
-
-cnt = 0
-for i in range(1, n + 1):
-  for j in range(1, n + 1):
-    if all(a[i][j] > a[i + dy[k]][j + dx[k]] for k in range(4)):   
-      cnt += 1
-
-print(cnt)
+print(count)
